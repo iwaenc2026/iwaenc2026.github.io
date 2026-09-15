6f2ef8340d082ce230f392212c3c3fe6968e13e9
Riccardo Giampiccolo <riccardo.giampiccolo@polimi.it>
2026-09-15 12:46:45 +0200

    Update hero.js

--- src/config/pages/gallery/hero.js
+++ src/config/pages/gallery/hero.js
@@ -2,7 +2,7 @@ const hero = {
   enabled: true,
   tagline: 'Workshop',
   title: 'Gallery',
-  subtitle: 'A collection of photos of IWAENC 2026.',
+  subtitle: 'A collection of photos of IWAENC 2026. Due to the high image resolution, it takes a few minutes to load all of them.',
   actions: [],
 };
 
